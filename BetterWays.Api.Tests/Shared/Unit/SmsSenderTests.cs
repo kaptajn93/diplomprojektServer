825a5e1034412e94cde7e56e6111a797a02899f9
Mikkel Hempel <mikkel.hempel@gmail.com>
2016-02-24 15:10:46 +0100

    Re-added SMS Sender

---
+++ BetterWays.Api.Tests/Shared/Unit/SmsSenderTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BetterWays.Api.Bounded_Contexts.Shared.Infrastructure;
+
+namespace BetterWays.Api.Tests.Shared.Unit
+{
+    [TestClass]
+    public class SmsSenderTests
+    {
+        [TestMethod]
+        public void CanSendSms()
+        {
+            // ARRANGE
+            var authId = "MAMZG2OGU1ZJZIZDC0ZW";
+            var authToken = "MDhiMWI5Y2U2ZTRjZTdkODFjOTQzNTY3YTk5OGVk";
+
+            var smsSender = new SmsSenderPlivo(authId, authToken);
+
+            // ACT
+            // This will charge MIHs personal account, so please use with care :)
+            //smsSender.SendSms("+4553747204", "Hello from BetterWays Unit Test.");
+
+            // ASSERT
+        }
+    }
+}
---
+++ BetterWays.Api/Bounded Contexts/Shared/Application/ISmsSender.cs
@@ -0,0 +1,7 @@
+﻿namespace BetterWays.Api.Bounded_Contexts.Shared.Application
+{
+    public interface ISmsSender
+    {
+        void SendSms(string receiver, string content);
+    }
+}
