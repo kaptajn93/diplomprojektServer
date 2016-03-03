07fb3cb5e0fc1531e56ad1e57a7fe275462f7321
Mikkel Hempel <mikkel.hempel@gmail.com>
2016-03-03 23:07:59 +0100

    Send email on video publish (WIP)

---
+++ BetterWays.Api/Bounded Contexts/CoachingCourses/Infrastructure/Controllers/ExerciseController.cs
@@ -0,0 +1,26 @@
+﻿using BetterWays.Api.Bounded_Contexts.Shared.Infrastructure;
+using System.Web.Http;
+
+namespace BetterWays.Api.Bounded_Contexts.CoachingCourses.Infrastructure.Controllers
+{
+    public class ExerciseController : ApiController
+    {
+        // POST api/exercise
+        public bool Post([FromBody]string videoUuid)
+        {
+            var apiKey = "live_d54b425a127f44b8e0f60904867d9b57aeaa9775";
+
+            var emailSender = new EmailSenderSendWithUs(apiKey);
+
+            var data = new
+            {
+                Username = "Mikkel",
+                VideoUUID = videoUuid
+            };
+
+            emailSender.SendEmail("mih@miracle.dk", data);
+
+            return true;
+        }
+    }
+}
---
+++ BetterWays.Api/Bounded Contexts/Shared/Infrastructure/EmailSenderSendWithUs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BetterWays.Api.Bounded_Contexts.Shared.Application;
+using SendWithUs.Client;
+
+namespace BetterWays.Api.Bounded_Contexts.Shared.Infrastructure
+{
+    public class EmailSenderSendWithUs : IEmailSender
+    {
+        // Find your Account Sid and Auth Token at twilio.com/user/account
+        private string _apiKey;
+
+        public EmailSenderSendWithUs(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        public async void SendEmail(string receiver, object content)
+        {
+            var request = new SendRequest
+            {
+                TemplateId = "tem_LqNdYMYNxutJdjHzkEQyvT",
+                SenderName = "BetterWays",
+                SenderAddress = "no-reply@betterways.com",
+                RecipientAddress = receiver,
+                Data = content
+            };
+
+            var client = new SendWithUsClient(_apiKey);
+            var response = await client.SendAsync(request);
+
+            if (response.Success)
+            {
+                Console.WriteLine("Email Sent");
+            }
+            else
+            {
+                Console.WriteLine(response.ErrorMessage);
+            }
+        }
+    }
+}
